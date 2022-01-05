466eea88b95c7b499bcf909412835d021cf1edb5
sgDaddy1995 <mark15071995@gmail.com>
2022-01-05 11:01:24 +0900

    -2022-01-05: Refactoring code to beautify

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,9 @@
-let inputLength = 0;
-let pattern = [];
-let isGameOver = true;
-let isInit = true;
-let level = 0;
-
+const textGameOver = "Game over, Press Any Key to Restart";
 const audioGreen = new Audio("sounds/green.mp3");
 const audioRed = new Audio("sounds/red.mp3");
 const audioYellow = new Audio("sounds/yellow.mp3");
 const audioBlue = new Audio("sounds/blue.mp3");
 const wrong = new Audio("sounds/wrong.mp3");
-
 const btn = {
   "green": audioGreen,
   "red": audioRed,
@@ -17,20 +11,22 @@ const btn = {
   "blue": audioBlue
 };
 
+let inputLength = 0;
+let level = 0;
+let pattern = [];
+let isGameOver = true;
+let isInit = true;
+
 let generateRandomColor = () => {
   return Object.keys(btn)[Math.floor(4 * Math.random())];
 }
 
-let resetGame = () => {
-  input = [];
-  pattern = [];
+let reset = (textStatus) => {
+  inputLength = 0;
   level = 0;
+  pattern = [];
   isGameOver = false;
-  updateStatus("Game over, Press Any Key to Restart");
-}
-
-let compareResult = (userInput, expectedPattern) => {
-  return JSON.stringify(userInput) === JSON.stringify(expectedPattern);
+  updateStatus(textStatus);
 }
 
 let updateStatus = (gameStatus) => {
@@ -39,57 +35,49 @@ let updateStatus = (gameStatus) => {
 
 $(document).on("keypress", function() {
   if (isGameOver) {
-    // Reset the game
-    resetGame();
     // Initialize new game
     level++;
-    $("h1").text(`Level ${level}`);
-    // Randomly click a button
+    // Reset
+    reset(`Level ${level}`);
+    // Randomly click a button and add its color to pattern
     let randomColor = generateRandomColor();
-    pattern.push(randomColor);
     let selector = `div.btn.${randomColor}`;
     $(selector).fadeOut(100).fadeIn(100);
     btn[`${randomColor}`].play();
+    pattern.push(randomColor);
   }
 });
 
 $("div.btn").on("click", function () {
-  if (isGameOver) {
-    $(this).fadeOut(100).fadeIn(100);
+  $(this).fadeOut(100).fadeIn(100); // Simulating button click
+  if ( isGameOver ) {
+    wrong.play();
     $("body").addClass("game-over");
     setTimeout(() => {
       $("body").removeClass("game-over");
     }, 200);
     return;
   }
-
-  // If the game is not over => Simulating the button click
-  $(this).fadeOut(100).fadeIn(100);
-  // Play button sound
-  btn[`${$(this).attr("id")}`].play();
-
-  console.log(pattern);
-  console.log(inputLength);
-  console.log($(this).attr("id"));
-  inputLength++;
-  if ($(this).attr("id") !== pattern[Math.max(inputLength-1, 0)]) {
-    isGameOver = true;
-    wrong.play();
-    resetGame();
+  btn[`${$(this).attr("id")}`].play();// Play sound if game can be continued
+  inputLength++; // inputLength is used to determine which element in the pattern to be compared with the current user input
+  if ( $(this).attr("id") !== pattern[Math.max(inputLength-1, 0)] ) { // Comparing the current input value with the last element of pattern
     $("body").addClass("game-over");
     setTimeout(() => {
       $("body").removeClass("game-over");
     }, 200);
+    wrong.play();
+    reset(textGameOver);
+    isGameOver = true;
   } else {
-    if (inputLength === pattern.length) { // User input matches the expected pattern
+    if ( inputLength === pattern.length ) { // User input matches the expected pattern
       inputLength = 0; // Reset user input
       let randomColor = generateRandomColor();
-      pattern.push(randomColor);
       let selector = `div.btn.${randomColor}`;
       setTimeout(() => {
         $(selector).fadeOut(100).fadeIn(100);
         btn[`${randomColor}`].play();
       }, 300)
+      pattern.push(randomColor);
       level++;
       updateStatus(`Level ${level}`);
     }
